89c8e2dd94911b943bccbe234a7d4f8e60644cf8
Alex Olkhovyk <oleksandr.olkhovyk.s@gmail.com>
2024-08-26 12:28:52 +0300

    add close window command

--- Task/ViewModels/MainViewModel.cs
+++ Task/ViewModels/MainViewModel.cs
@@ -1,9 +1,47 @@
-﻿namespace Task.ViewModels;
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Task.ViewModels;
 
 public class MainViewModel : BindableBase
 {
+
+
+    #region Commands
+
+    public ICommand CloseCommand { get; set; }
+    public ICommand MoveWindowCommand { get; set; }
+    
+    public ICommand SaveCommand { get; set; }
+    public ICommand CancelCommand { get; set; }
+    public ICommand ImportFileCommand { get; set; }
+    public ICommand ExportFileCommand { get; set; }
+    public ICommand AddRowCommand { get; set; }
+    public ICommand CopyRowCommand { get; set; }
+    public ICommand RemoveRowCommand { get; set; }
+
+    #endregion
+
     public MainViewModel()
     {
-        
+        #region Init commands
+        CloseCommand = new DelegateCommand(
+            () => Application.Current.Shutdown(), 
+            () => true);
+
+        MoveWindowCommand = new DelegateCommand(
+            () => Application.Current.MainWindow.DragMove(),
+            () => true);
+
+        #endregion
+
     }
+
+
+
+    #region Private Methods
+
+    
+
+    #endregion
 }
