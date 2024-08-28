d7d16a086c07a63b0a3c75a2d123c4f8fb7578ca
Alex Olkhovyk <oleksandr.olkhovyk.s@gmail.com>
2024-08-28 20:02:40 +0300

    Create helpers for validation columns

---
+++ Task/Helpers/DataGridCellValitationRule.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using DryIoc.ImTools;
+
+namespace Task.Helpers;
+
+public class DataGridCellValitationRule : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        var cellValue = value.ToString();
+
+        if(cellValue.Length > 10)
+            return new ValidationResult(false, "Value can`t be empty and lenght can`t be more than 10!");
+        if ( string.IsNullOrWhiteSpace(cellValue))
+            return new ValidationResult(false, "Value can`t be empty and lenght can`t be more than 10!");
+        
+        
+        return ValidationResult.ValidResult;
+    }
+}
+
---
+++ Task/Infrastructure/AutoGeneratingColumnBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace Task.Infrastructure;
+
+
+public class AutoGeneratingColumnBehavior : Behavior<DataGrid>
+{
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register("Command", typeof(ICommand), typeof(AutoGeneratingColumnBehavior), new PropertyMetadata(null));
+
+    public ICommand Command
+    {
+        get { return (ICommand)GetValue(CommandProperty); }
+        set { SetValue(CommandProperty, value); }
+    }
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.AutoGeneratingColumn += OnAutoGeneratingColumn;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject.AutoGeneratingColumn -= OnAutoGeneratingColumn;
+    }
+
+    private void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+    {
+        if (Command != null && Command.CanExecute(e))
+        {
+            Command.Execute(e);
+        }
+    }
+}
